cef330bdf15f3827725fca248379082bd191015a
01farfan <timarello@gmail.com>
2022-01-06 04:42:37 -0500

    Second commit

--- src/Test.java
+++ src/Test.java
@@ -4,8 +4,7 @@ public class Test {
 
         System.out.println("First");
         System.out.println("Second");
-        System.out.println("Third");
-        System.out.println("Forth");
+
 
 
     }
